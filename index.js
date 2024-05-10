9c107d7b837abc0a19a30eb8d764109ad41cdbf2
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-05-10 03:47:17 +0200

    added a try-catch block that will display an error if the incorrect unit of measurement is used

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 const distance2 = distance + (velocity * (time / 3600)) //calculates new distance
 const remainingFuel = fuel - (fuelBurnRate * time) //calculates remaining fuel
 
-const newAcceleration = acceleration * (3600 / 1000); // converting m/s^2 to km/h^2
+const newAcceleration = acceleration * (3600 / 1000) ; // converting m/s^2 to km/h^2
 
 const velocity2 = calculateNewVelocity(velocity, newAcceleration, time) //calculates new velocity based on acceleration
 
@@ -24,6 +24,14 @@ const velocity2 = calculateNewVelocity(velocity, newAcceleration, time) //calcul
 function calculateNewVelocity  (velocity, newAcceleration, time) {
   return velocity + (newAcceleration * time) // calculate the new velocity based on the initial velocity (vel), the converted acceleration (newAcc), and the time (time).
 }
+try {
+  if (calculateNewVelocity(velocity, newAcceleration, time) !== velocity + (newAcceleration * time))
+    {
+      throw new Error('Incorrect unit of measurement used!')
+    }
+} catch (error) {
+  console.error(`Error:${error.message}`)
+} 
 
 console.log(`Corrected New Velocity: ${velocity2} km/h`);
 console.log(`Corrected New Distance: ${distance2} km`);
